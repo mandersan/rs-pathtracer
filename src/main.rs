9c5f1e05de1085325b162a87d0c9bb8491f008af
Mark Anderson <mark@illumedevelopments.co.uk>
2018-01-30 13:43:41 +0000

    PNG output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+extern crate lodepng;
+
 use std::fs::File;
 use std::io::Write;
 
@@ -8,10 +10,11 @@ fn main() {
 
     let width = 320;
     let height = 256;
+    
+    let mut image: Vec<u8> = Vec::new();
+
     let ppm_max_value = 255;
-
     let ppm_header = format!("P3\n{} {}\n{}\n", width, height, ppm_max_value);
-
     let mut output_file = File::create("output.ppm")
         .expect("Unable to open output file");
     output_file.write_all(ppm_header.as_bytes())
@@ -25,7 +28,14 @@ fn main() {
             let element = format!("{} {} {}\n", r, g, b);
             output_file.write_all(element.as_bytes())
                 .expect("Unable to write element");
+
+            image.push(r as u8);
+            image.push(g as u8);
+            image.push(b as u8);
         }
     }
+
+    lodepng::encode_file("output.png", image.as_slice(), width, height, lodepng::ColorType::RGB, 8)
+        .expect("Unable to save PNG");
 }
 
