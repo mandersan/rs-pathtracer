5f3b6395796285321c45303b0e8a3ccd0d8c17f8
Mark Anderson <mark@illumedevelopments.co.uk>
2018-02-15 14:17:57 +0000

    WIP

--- src/main.rs
+++ src/main.rs
@@ -6,42 +6,27 @@ use std::io::Write;
 use cgmath::*;
 
 struct Camera {
-    eye: Vector3<f32>,
-    target: Vector3<f32>,
+    eye: Point3<f32>,
+    target: Point3<f32>,
     up: Vector3<f32>,
     fov: Deg<f32>,
     near: f32,
     far: f32,
 }
 
-fn look_at(eye: Vector3<f32>, target: Vector3<f32>, up: Vector3<f32>) -> Matrix4<f32> {
-    let axis_z = (target - eye).normalize();
-    let axis_x = axis_z.cross(up).normalize();
-    let axis_y = axis_x.cross(axis_z);
-    return Matrix4::from_cols(
-        vec4(axis_x.x, axis_y.x, -axis_z.x, 0.),
-        vec4(axis_x.y, axis_y.y, -axis_z.y, 0.),
-        vec4(axis_x.z, axis_y.z, -axis_z.z, 0.),
-        vec4(-dot(axis_x, eye), -dot(axis_y, eye), dot(axis_z, eye), 1.)
-    );
-}
-
-
 fn main() {
-    let tmp = vec3(0.0,0.0,0.0) - vec3(0.0,0.0,0.0);
-
     // :TODO:
     // - Preview output in window.
     // - Command line args parsing (output to file/window).
     // - 
 
-    let image_width = 320;
-    let image_height = 256;
+    let image_width = 4;//320;
+    let image_height = 4;//256;
     let image_aspect = image_width as f32 / image_height as f32;
 
     let camera = Camera {
-        eye: vec3(0., 0., 0.),
-        target: vec3(0., 0., -1.),
+        eye: Point3::new(0., 0., 0.),
+        target: Point3::new(0., 0., -1.),
         up: vec3(0., 1., 0.),
         fov: Deg(60.0),
         near: 0.01,
@@ -49,12 +34,10 @@ fn main() {
     };
 
     // :TODO: http://www.songho.ca/opengl/gl_transform.html
-    //  Possible todo the perspective divide & conversion to window coords in a matrix?
-    let screen_matrix = Matrix4::zero();
-    let view_matrix = look_at(camera.eye, camera.target, camera.up);
+    let view_matrix = Matrix4::look_at(camera.eye, camera.target, camera.up);
     let projection_matrix = perspective(camera.fov, image_aspect, camera.near, camera.far);
-    let screen_view_projection_matrix = screen_matrix * projection_matrix * view_matrix;
-    let inv_screen_view_projection_matrix = screen_view_projection_matrix.inverse_transform();
+    let view_projection_matrix = projection_matrix * view_matrix;
+    let inv_view_projection_matrix = view_projection_matrix.inverse_transform().unwrap();
 
     /*
         In: (screenX, screenY)
@@ -66,14 +49,7 @@ fn main() {
         Out: (worldX, worldY, worldZ)
     */
 
-
-
-    // :TODO: Viewport matrix (using 'lookAt' function)
-    //      float4x4 viewMatrix = lookat(CamPos, TgtPos, float3{ 0.f, 1.f, 0.f });
-    // :TODO: For each pixel, build ray (NDC? So, -1 to 1 in each axis) and transform using viewProj matrix.
-    //      Basically screen space (0..width, 0..height) to world space.
     // :TODO: Get simple sky & sphere intersection working.
-
 
     
     let mut image: Vec<u8> = Vec::new();
@@ -87,6 +63,19 @@ fn main() {
 
     for y in 0..image_height {
         for x in 0..image_width {
+            let ndc = Point3::new(
+                (x as f32 / ((image_width - 1) as f32 / 2.)) - 1.,
+                (y as f32 / ((image_height - 1) as f32 / 2.)) - 1.,
+                0.
+            );
+
+            let ray_pos = inv_view_projection_matrix.transform_point(ndc);
+            //let ray_pos = inv_view_projection_matrix * ndc;
+            let ray_dir = inv_view_projection_matrix.transform_vector(vec3(0., 0., -1.));
+
+            println!("({}, {}): {:?} {:?} {:?}", x, y, ndc, ray_pos, ray_dir);
+
+
             let r = (x % 16) * 16;
             let g = 0;
             let b = (y % 16) * 16;
