2b8d8ef716eda54f7314e1c625b09fa74b2dd14e
Mark Anderson <mark@illumedevelopments.co.uk>
2018-02-16 17:47:38 +0000

    Sphere intersections

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,8 @@ extern crate cgmath;
 
 use image::ColorType;
 use image::png::PNGEncoder;
+use std::f32;
 use std::fs::File;
-// use std::io::Write;
 use cgmath::*;
 
 struct Camera {
@@ -25,6 +25,60 @@ fn write_png_rgb8(filename: &str, pixels: &[u8], dimensions: (u32, u32))
     Ok(())
 }
 
+struct Sphere {
+    origin: Point3<f32>,
+    radius: f32
+}
+
+struct Ray {
+    origin: Point3<f32>,
+    direction: Vector3<f32>
+}
+
+struct Interval {
+    min: f32,
+    max: f32
+}
+
+struct Hit {
+    distance: f32,
+    location: Point3<f32>,
+    normal: Vector3<f32>
+}
+
+fn intersect_test_sphere(sphere: &Sphere, ray: &Ray, interval: &Interval) -> Option<Hit>
+{
+    let sphere_to_ray_origin = ray.origin - sphere.origin;
+    let a = dot(ray.direction, ray.direction);
+    let b = dot(sphere_to_ray_origin, ray.direction);
+    let c = dot(sphere_to_ray_origin, sphere_to_ray_origin) - (sphere.radius * sphere.radius);
+    let discriminant = b * b - a * c;
+
+    //println!("{:?} {:?} {:?} {:?} {} {} {} {}", ray.origin, ray.direction, sphere.origin, sphere_to_ray_origin, a, b, c, discriminant);
+
+    if discriminant > 0. {
+        let tmp = (-b - (b * b - a * c).sqrt()) / a;
+        if tmp < interval.max && tmp > interval.min {
+            let hit_location = ray.origin + (tmp * ray.direction);
+            return Some(Hit {
+                distance: tmp,
+                location: hit_location,
+                normal: (hit_location - sphere.origin) / sphere.radius
+            });
+        }
+        let tmp = (-b + (b * b - a * c).sqrt()) / a;
+        if tmp < interval.max && tmp > interval.min {
+            let hit_location = ray.origin + (tmp * ray.direction);
+            return Some(Hit {
+                distance: tmp,
+                location: hit_location,
+                normal: (hit_location - sphere.origin) / sphere.radius
+            });
+        }
+    }
+    None    
+}
+
 fn main() {
     // :TODO:
     // - Preview output in window.
@@ -36,7 +90,7 @@ fn main() {
     let image_aspect = image_width as f32 / image_height as f32;
 
     let camera = Camera {
-        eye: Point3::new(0., 0., 0.),
+        eye: Point3::new(0., 0., 2.),
         target: Point3::new(0., 0., 1.),
         up: vec3(0., 1., 0.),
         fov: Deg(60.0),
@@ -44,27 +98,15 @@ fn main() {
         far: 100.,
     };
 
-    // :TODO: http://www.songho.ca/opengl/gl_transform.html
     let view_matrix = Matrix4::look_at(camera.eye, camera.target, camera.up);
     let projection_matrix = perspective(camera.fov, image_aspect, camera.near, camera.far);
     let view_projection_matrix = projection_matrix * view_matrix;
     let inv_view_projection_matrix = view_projection_matrix.inverse_transform().unwrap();
-
     //println!("view: {:?}\nproj: {:?}\nviewProj: {:?}\ninvViewProj: {:?}", view_matrix, projection_matrix, view_projection_matrix, inv_view_projection_matrix);
 
-
-    // :TODO: Get simple sky & sphere intersection working.
-
+    let test_sphere = Sphere { origin: Point3::new(0., 0., 1.), radius: 0.3 };
     
     let mut image: Vec<u8> = Vec::new();
-
-    // Test PPM output
-    // let ppm_max_value = 255;
-    // let ppm_header = format!("P3\n{} {}\n{}\n", image_width, image_height, ppm_max_value);
-    // let mut output_file = File::create("output.ppm")
-    //     .expect("Unable to open output file");
-    // output_file.write_all(ppm_header.as_bytes())
-    //     .expect("Error writing header");
 
     for y in 0..image_height {
         for x in 0..image_width {
@@ -73,25 +115,32 @@ fn main() {
                 (y as f32 / ((image_height - 1) as f32 / 2.)) - 1.,
                 0.
             );
-
             let ray_pos = inv_view_projection_matrix.transform_point(ndc);
             let ray_dir = (ray_pos - camera.eye).normalize();
-
             //println!("({}, {}): {:?} {:?} {:?}", x, y, ndc, ray_pos, ray_dir);
 
-            let t = 0.5 * (ray_dir.y + 1.0);
-            let colour = ((1.0 - t) * vec3(1., 1., 1.)) + (t * vec3(0.5, 0.7, 1.0));
+            let ray = Ray {
+                origin: ray_pos,
+                direction: ray_dir
+            };
+
+            let hit = intersect_test_sphere(&test_sphere, &ray, &Interval { min: 0.0, max: f32::MAX });
+            let colour = match hit {
+                None => {
+                    let t = 0.5 * (ray_dir.y + 1.0);
+                    ((1.0 - t) * vec3(1., 1., 1.)) + (t * vec3(0.5, 0.7, 1.0))
+                },
+                Some(hit) => {
+                    // :NOTE: Just showing normals
+                    0.5 * vec3(hit.normal.x + 1., hit.normal.y + 1., hit.normal.z + 1.)
+                }
+            };
 
             //let Vector3 { x: r, y: g, z: b} = colour;
 
             let r = colour.x * 255.;
             let g = colour.y * 255.;
             let b = colour.z * 255.;
-
-            // Test PPM output
-            // let element = format!("{} {} {}\n", r, g, b);
-            // output_file.write_all(element.as_bytes())
-            //     .expect("Unable to write element");
 
             image.push(r as u8);
             image.push(g as u8);
