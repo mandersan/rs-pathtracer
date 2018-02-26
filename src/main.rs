cd3030e6ebfe02203a101a72b5848ae0e3319e64
Mark Anderson <mark@illumedevelopments.co.uk>
2018-02-26 17:20:06 +0000

    Simple camera animation

--- src/main.rs
+++ src/main.rs
@@ -17,8 +17,8 @@ extern crate sdl2;
 mod raytracing;
 
 use cgmath::*;
-use image::ColorType;
-use image::png::PNGEncoder;
+// use image::ColorType;
+// use image::png::PNGEncoder;
 use rand::{random};
 use raytracing::materials::{Dialectric, Lambertian, Metal};
 use raytracing::{Hitable, Ray};
@@ -28,105 +28,44 @@ use sdl2::pixels::PixelFormatEnum;
 use sdl2::rect::Rect;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
-use std::fs::File;
+// use std::fs::File;
 
-fn write_png_rgb8(filename: &str, pixels: &[u8], dimensions: (u32, u32))
-    -> Result<(), std::io::Error>
-{
-    let output = File::create(filename)?;
-    let encoder = PNGEncoder::new(output);
-    encoder.encode(pixels, dimensions.0, dimensions.1, ColorType::RGB(8))?;
-    Ok(())
-}
+// fn write_png_rgb8(filename: &str, pixels: &[u8], dimensions: (u32, u32))
+//     -> Result<(), std::io::Error>
+// {
+//     let output = File::create(filename)?;
+//     let encoder = PNGEncoder::new(output);
+//     encoder.encode(pixels, dimensions.0, dimensions.1, ColorType::RGB(8))?;
+//     Ok(())
+// }
 
 fn main() {
     // Set up image output & camera
-    let image_width = 640;
-    let image_height = 400;
+    let window_width = 640;
+    let window_height = 480;
+    let image_width = 320;
+    let image_height = 240;
     let image_aspect = image_width as f32 / image_height as f32;
-    let num_samples = 32;
-    let camera = raytracing::cameras::util::create_camera(
-        Point3::new(0., 0.2, 0.75),
-        Point3::new(0., 0., -1.),
-        vec3(0., 1., 0.),
-        Deg(60.),
-        0.01,
-        100.,
-        0.2
-    );
-    let view_matrix = Matrix4::look_at(camera.eye, camera.target, camera.up);
-    let projection_matrix = perspective(camera.fov, image_aspect, camera.near, camera.far);
-    let view_projection_matrix = projection_matrix * view_matrix;
-    let inv_view_projection_matrix = view_projection_matrix.inverse_transform().unwrap();
+    let num_samples = 2;
 
     // Build scene
     // :TODO: Think further about how to represent a collection of hetergenous objects uniformly.
     let mut shapes: Vec<Box<Hitable>> = Vec::new();
-    shapes.push(Box::new(Sphere { origin: Point3::new(0., 0., -1.), radius: 0.5, material: Box::new(Lambertian { albedo: vec3(0.1, 0.2, 0.5) }) }));
+    shapes.push(Box::new(Sphere { origin: Point3::new(0., 0., 0.), radius: 0.5, material: Box::new(Lambertian { albedo: vec3(0.1, 0.2, 0.5) }) }));
     shapes.push(Box::new(Plane { origin: Point3::new(0., -0.5, 0.), normal: vec3(0., 1., 0.), material: Box::new(Lambertian { albedo: vec3(0.2, 0.5, 0.2) }) }));
-    shapes.push(Box::new(Sphere { origin: Point3::new(1., 0., -1.), radius: 0.5, material: Box::new(Metal { albedo: vec3(0.8, 0.6, 0.2), fuzziness: 0.3 }) }));
-    shapes.push(Box::new(Sphere { origin: Point3::new(-1., 0., -1.), radius: 0.5, material: Box::new(Dialectric { refractive_index: 1.5 }) }));
-    //shapes.push(Box::new(Sphere { origin: Point3::new(-1., 0., -1.), radius: -0.45, material: Box::new(Dialectric { refractive_index: 1.5 }) }));
-
-    // Raytrace scene
-    let mut image: Vec<u8> = Vec::new();
-    for y in 0..image_height {
-        for x in 0..image_width {
-            let mut colour = Vector3::zero();
-            for _s in 0..num_samples {
-                let sx = (x as f32) + random::<f32>();
-                let sy = (y as f32) + random::<f32>();
-
-                 let ndc = Point3::new(
-                    (sx as f32 / (image_width as f32 / 2.)) - 1.,
-                    (-(sy as f32) / (image_height as f32 / 2.)) + 1.,
-                    0.
-                );
-                let ray_pos = inv_view_projection_matrix.transform_point(ndc);
-                let ray_dir = (ray_pos - camera.eye).normalize();
-
-                // :TODO: Defocus blur - tidy up, move some logic into camera struct
-                let lens_radius = camera.aperture / 2.;
-                let rd = lens_radius * random::random_in_unit_disk();
-                let cam_up = view_matrix.transform_vector(vec3(0., 1., 0.));
-                let cam_right = ray_dir.cross(cam_up);
-                let ray_offset = (cam_up * rd.x) + (cam_right * rd.y);
-                let focus_point = ray_pos + (ray_dir * camera.focal_distance);
-                let ray_pos = ray_pos + ray_offset;
-                let ray_dir = (focus_point - ray_pos).normalize();
-
-                let ray = Ray {
-                    origin: ray_pos,
-                    direction: ray_dir
-                };
-
-                colour += raytracing::tracing::trace(&shapes, &ray, 0);
-            }
-            colour = colour / (num_samples as f32);
-
-            // Gamma correct & convert to 8bpp
-            let colour = vec3(colour.x.sqrt(), colour.y.sqrt(), colour.z.sqrt()) * 255.;
-            let Vector3 { x: r, y: g, z: b} = colour;
-
-            image.push(r as u8);
-            image.push(g as u8);
-            image.push(b as u8);
-        }
-    }
-
-    // Save image as file
-    write_png_rgb8("output.png", image.as_slice(), (image_width, image_height)).expect("Unable to save PNG");
+    shapes.push(Box::new(Sphere { origin: Point3::new(1., 0., 0.), radius: 0.5, material: Box::new(Metal { albedo: vec3(0.8, 0.6, 0.2), fuzziness: 0.3 }) }));
+    shapes.push(Box::new(Sphere { origin: Point3::new(-1., 0., 0.), radius: 0.5, material: Box::new(Dialectric { refractive_index: 1.5 }) }));
+    //shapes.push(Box::new(Sphere { origin: Point3::new(-1., 0., 0.), radius: -0.45, material: Box::new(Dialectric { refractive_index: 1.5 }) }));
 
 
-
-    //-----
+    let mut cam_pos = Point3::new(0., 0.2, 1.75);
 
 
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo: Video", image_width, image_height)
+    let window = video_subsystem.window("rust-sdl2 demo: Video", window_width, window_height)
         .position_centered()
         //.opengl()
         .build()
@@ -134,25 +73,6 @@ fn main() {
 
     let mut canvas = window.into_canvas().build().unwrap();
     let texture_creator = canvas.texture_creator();
-
-    let mut texture = texture_creator.create_texture_streaming(
-        PixelFormatEnum::RGB24, image_width, image_height).unwrap();
-    // Create a red-green gradient
-    texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
-        for y in 0..image_height as usize {
-            for x in 0..image_width as usize {
-                let offset = y*pitch + x*3;
-                let offset_in = y*(image_width * 3) as usize + x*3;
-                buffer[offset + 0] = image[offset_in + 0];
-                buffer[offset + 1] = image[offset_in + 1];
-                buffer[offset + 2] = image[offset_in + 2];
-            }
-        }
-    }).unwrap();
-
-    canvas.clear();
-    canvas.copy(&texture, None, Some(Rect::new(0, 0, image_width, image_height))).unwrap();
-    canvas.present();
 
     let mut event_pump = sdl_context.event_pump().unwrap();
 
@@ -167,5 +87,87 @@ fn main() {
             }
         }
         // The rest of the game loop goes here...
+
+        let rotation = Matrix3::from_angle_y(Deg(1.));
+        cam_pos = rotation.transform_point(cam_pos);
+
+        let camera = raytracing::cameras::util::create_camera(
+            cam_pos,
+            Point3::new(0., 0., 0.),
+            vec3(0., 1., 0.),
+            Deg(60.),
+            0.01,
+            100.,
+            0.2
+        );
+        let view_matrix = Matrix4::look_at(camera.eye, camera.target, camera.up);
+        let projection_matrix = perspective(camera.fov, image_aspect, camera.near, camera.far);
+        let view_projection_matrix = projection_matrix * view_matrix;
+        let inv_view_projection_matrix = view_projection_matrix.inverse_transform().unwrap();
+
+        // Raytrace scene
+        let mut image: Vec<u8> = Vec::new();
+        for y in 0..image_height {
+            for x in 0..image_width {
+                let mut colour = Vector3::zero();
+                for _s in 0..num_samples {
+                    let sx = (x as f32) + random::<f32>();
+                    let sy = (y as f32) + random::<f32>();
+
+                    let ndc = Point3::new(
+                        (sx as f32 / (image_width as f32 / 2.)) - 1.,
+                        (-(sy as f32) / (image_height as f32 / 2.)) + 1.,
+                        0.
+                    );
+                    let ray_pos = inv_view_projection_matrix.transform_point(ndc);
+                    let ray_dir = (ray_pos - camera.eye).normalize();
+
+                    // :TODO: Defocus blur - tidy up, move some logic into camera struct
+                    let lens_radius = camera.aperture / 2.;
+                    let rd = lens_radius * random::random_in_unit_disk();
+                    let cam_up = view_matrix.transform_vector(vec3(0., 1., 0.));
+                    let cam_right = ray_dir.cross(cam_up);
+                    let ray_offset = (cam_up * rd.x) + (cam_right * rd.y);
+                    let focus_point = ray_pos + (ray_dir * camera.focal_distance);
+                    let ray_pos = ray_pos + ray_offset;
+                    let ray_dir = (focus_point - ray_pos).normalize();
+
+                    let ray = Ray {
+                        origin: ray_pos,
+                        direction: ray_dir
+                    };
+
+                    colour += raytracing::tracing::trace(&shapes, &ray, 0);
+                }
+                colour = colour / (num_samples as f32);
+
+                // Gamma correct & convert to 8bpp
+                let colour = vec3(colour.x.sqrt(), colour.y.sqrt(), colour.z.sqrt()) * 255.;
+                let Vector3 { x: r, y: g, z: b} = colour;
+
+                image.push(r as u8);
+                image.push(g as u8);
+                image.push(b as u8);
+            }
+        }
+
+        let mut texture = texture_creator.create_texture_streaming(
+            PixelFormatEnum::RGB24, image_width, image_height).unwrap();
+        // Create a red-green gradient
+        texture.with_lock(None, |buffer: &mut [u8], pitch: usize| {
+            for y in 0..image_height as usize {
+                for x in 0..image_width as usize {
+                    let offset = y*pitch + x*3;
+                    let offset_in = y*(image_width * 3) as usize + x*3;
+                    buffer[offset + 0] = image[offset_in + 0];
+                    buffer[offset + 1] = image[offset_in + 1];
+                    buffer[offset + 2] = image[offset_in + 2];
+                }
+            }
+        }).unwrap();
+
+        canvas.clear();
+        canvas.copy(&texture, None, Some(Rect::new(0, 0, window_width, window_height))).unwrap();
+        canvas.present();
     }
 }
