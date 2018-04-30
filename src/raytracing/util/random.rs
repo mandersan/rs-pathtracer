24a3478667a005bd36d9b827860a6ba1407a75bb
Mark Anderson <mark@bigfatface.co.uk>
2018-04-30 13:54:32 +0100

    - Better random unit vector.

--- src/raytracing/util/random.rs
+++ src/raytracing/util/random.rs
@@ -1,5 +1,6 @@
 use cgmath::*;
 use rand::{random};
+use std::f32;
 
 pub fn random_in_unit_sphere() -> Vector3<f32> {
     // :TODD: Check that the initial random vector has values in the range 0..1
@@ -21,8 +22,15 @@ pub fn random_in_unit_disk() -> Vector3<f32> {
 }
 
 pub fn random_unit_vector() -> Vector3<f32> {
+    let z = (2.0 * random::<f32>()) - 1.0;
+    let tmp = (1.0 - (z * z)).sqrt();
+    let azimuth = random::<f32>() * 2.0 * f32::consts::PI;
+    let x = azimuth.cos() * tmp;
+    let y = azimuth.sin() * tmp;
+    vec3(x, y, z)
+
     // :TODO: Not sure if this is correct
-    let p = (2. * random::<Vector3<f32>>()) - vec3(1., 1., 1.);
-    let tmp = 1.0 / p.mul_element_wise(p).sum().sqrt();
-    p * tmp
+    // let p = (2. * random::<Vector3<f32>>()) - vec3(1., 1., 1.);
+    // let tmp = 1.0 / p.mul_element_wise(p).sum().sqrt();
+    // p * tmp
 }
